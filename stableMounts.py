d07339495b244f6a2f828fdfaa2d2e27c21f6279
Æryan <116835956+Aryan-0001@users.noreply.github.com>
2024-09-25 22:09:48 +0530

    Solution to stable mountains problem
    Python

---
+++ stableMounts.py
@@ -0,0 +1,7 @@
+class Solution:
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        stable=[]
+        for i in range(1,len(height)):
+            if height[i-1]>threshold:
+                stable.append(i)
+        return stable
