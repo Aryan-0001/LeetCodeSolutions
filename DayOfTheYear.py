833eac6523db9857cdc56d87ee922b81e4c2c4e8
Æryan <116835956+Aryan-0001@users.noreply.github.com>
2025-02-27 18:56:50 +0530

    Delete DayOfTheYear.py

--- DayOfTheYear.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def dayOfYear(self, date: str) -> int:
-        year,month,day = map(int, date.split('-'))
-        if (year%400==0 or (year%100!=0 and year%4==0)) and month>2: 
-            day+=1
-        months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        for i in range(1, month):
-            day+=months[i-1]
-        return day
